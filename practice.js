6b4cd571f197da5dce0b1aaedcd77ae0096ccff7
joywamaitha <joy.wamaitha@sibelhealth.com>
2024-08-19 05:26:28 +0300

    Initial commit - Portfolio setup

---
+++ practice.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+var bookname="Utopia";
+var try1=bookname.length;
+console.log(try1);
+
+var sent="I love changing for the better";
+var try2=sent.indexOf("changing");
+console.log(try2);
